9416fd0ab49d24b6eddfe2e2b5ffb50477d5770b
Bret Miller <bret.miller2010@gmail.com>
2024-09-01 12:51:35 -0500

    added husky and main page updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,28 @@
-import Image from "next/image";
-import styles from '@/app/ui/home.module.css'
+"use client";
+import {
+  Title,
+  MainWrapper,
+  GraphWrapper,
+  CoinHeaderWrapper,
+  CoinStatsWrapper,
+} from "./styles";
 
 export default function Home() {
   return (
     <main>
-      <div>Test</div>
+      <div>
+        <MainWrapper>
+          <CoinHeaderWrapper>
+            This will be where the coin header goes
+          </CoinHeaderWrapper>
+          <GraphWrapper>
+            <Title>This will be where the graphs go</Title>
+          </GraphWrapper>
+          <CoinStatsWrapper>
+            This is where the full list of coins will go
+          </CoinStatsWrapper>
+        </MainWrapper>
+      </div>
     </main>
   );
 }
---
+++ src/app/styles.tsx
@@ -0,0 +1,34 @@
+"use client";
+import styled from "styled-components";
+
+export const Title = styled.h1`
+  background-color: red;
+`;
+
+export const MainWrapper = styled.div`
+  border: 1px solid red;
+  height: 1000px;
+  margin-left: 15px;
+  margin-right: 15px;
+`;
+
+export const GraphWrapper = styled.div`
+  margin-top: 50px;
+  background-color: white;
+  border: 1px solid blue;
+  height: 100px;
+`;
+
+export const CoinHeaderWrapper = styled.div`
+  margin-top: 50px;
+  background-color: green;
+  border: 1px solid red;
+  height: 100px;
+`;
+
+export const CoinStatsWrapper = styled.div`
+  margin-top: 50px;
+  background-color: blue;
+  border: 1px solid red;
+  height: 300px;
+`;
