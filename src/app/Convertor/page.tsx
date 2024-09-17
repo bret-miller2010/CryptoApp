a8d13b1c7780529db004bfbbff559b87c6d86fdf
Bret Miller <bret.miller2010@gmail.com>
2024-09-17 16:35:13 -0500

    More updates to the convertor page. Pulling info to show coin data

--- src/app/Convertor/page.tsx
+++ src/app/Convertor/page.tsx
@@ -1,8 +1,49 @@
 "use client";
+import { getInformation } from "../api";
 import { useState, useEffect } from "react";
 
 export default function Compare() {
   const [currentDate, setCurrentDate] = useState("");
+  const [MarketData, setMarketData] = useState([]);
+  const [ErrorMessage, setErrorMessage] = useState("");
+
+  const CompareBox = () => {
+    const [coinInfo, setCoinInfo] = useState("");
+
+    const setDisplayInfo = (coin) => {
+      setCoinInfo(coin.target.value);
+    };
+
+    return (
+      <div>
+        <select
+          onChange={setDisplayInfo}
+          className="w-24 text-black"
+          name=""
+          id=""
+        >
+          {MarketData.map((coin) => (
+            <option key={coin["id"]} className="text-black" value={coin["id"]}>
+              {coin["name"]}
+            </option>
+          ))}
+        </select>
+        <div>{coinInfo}</div>
+      </div>
+    );
+  };
+
+  const getCoinData = async () => {
+    const coinKey =
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=true&price_change_percentage=1h%2C24h%2C7d";
+    try {
+      const coinData = await getInformation(coinKey);
+      setErrorMessage("");
+      setMarketData(coinData);
+    } catch (e) {
+      setErrorMessage("Could not load crypto currency data. Please try again.");
+    }
+  };
 
   useEffect(() => {
     const current = new Date();
@@ -12,17 +53,23 @@ export default function Compare() {
     const hours = current.getHours();
     const minutes = current.getMinutes();
     setCurrentDate(`${month + 1}/${day}/${year} | ${hours}:${minutes}`);
+    getCoinData();
   }, []);
 
   return (
     <div className="text-white mx-[15px]">
       <div className="flex flex-col mt-5">
+        <div>{ErrorMessage}</div>
         <div>Online currency convertor</div>
         <div>{currentDate}</div>
 
         <div className="flex justify-center my-5">
-          <div className="w-5/12 h-[150px] rounded-3xl p-5 bg-[#181825] mr-5">You sell</div>
-          <div className="w-5/12 h-[150px] rounded-3xl p-5 bg-[#181825] ml-5">You Buy</div>
+          <div className="w-5/12 h-[150px] rounded-3xl p-5 bg-[#181825] mr-5">
+            <CompareBox />
+          </div>
+          <div className="w-5/12 h-[150px] rounded-3xl p-5 bg-[#181825] ml-5">
+            <CompareBox />
+          </div>
         </div>
         <div className="flex justify-center items-center h-[500px] rounded-3xl bg-[#181825]">
           Graph for value
