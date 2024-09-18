92c2a618549556eff485845e022d9ddba22b089d
Bret Miller <bret.miller2010@gmail.com>
2024-09-18 13:06:42 -0500

    Finished convertor page for right now. Need to research how to add the graph components just like for the main page. Questions regarding input delay

--- src/app/Convertor/page.tsx
+++ src/app/Convertor/page.tsx
@@ -1,37 +1,128 @@
 "use client";
 import { getInformation } from "../api";
+import PropTypes from "prop-types";
 import { useState, useEffect } from "react";
+
+const Container = ({ marketData }) => {
+  const [leftSelection, setLeftSelection] = useState(marketData[0]);
+  const [rightSelection, setRightSelection] = useState(marketData[1]);
+  const [sellAmount, setSellAmount] = useState(1);
+  const [buyAmount, setBuyAmount] = useState(1);
+
+  const setLeft = (value) => {
+    const selectedCoin = value.target.value;
+    const pickedCoin = marketData.find((coin) => coin.id === selectedCoin);
+    setLeftSelection(pickedCoin);
+  };
+
+  const setRight = (value) => {
+    const selectedCoin = value.target.value;
+    const pickedCoin = marketData.find((coin) => coin.id === selectedCoin);
+    setRightSelection(pickedCoin);
+  };
+
+  const numberToSell = (value) => {
+    const coinToSell = value.target.value;
+    const leftCoinPrice = leftSelection?.current_price;
+    const rightCoinPrice = rightSelection?.current_price;
+    setSellAmount(coinToSell);
+    setBuyAmount((leftCoinPrice / rightCoinPrice) * sellAmount);
+  };
+
+  const numberToBuy = (value) => {
+    const coinToBuy = value.target.value;
+    const leftCoinPrice = leftSelection?.current_price;
+    const rightCoinPrice = rightSelection?.current_price;
+    setBuyAmount(coinToBuy);
+    setSellAmount((rightCoinPrice / leftCoinPrice) * buyAmount);
+  };
+
+  return (
+    <div className="flex justify-center my-5">
+      <div className="w-5/12 h-[150px] rounded-3xl p-5 bg-[#181825] mr-5">
+        <div>You sell</div>
+        <div className="flex justify-between">
+          <select onChange={setLeft} className="w-24 text-black" name="" id="">
+            {marketData.map((coin) => (
+              <option
+                key={coin["id"]}
+                className="text-black"
+                value={coin["id"]}
+              >
+                {coin["name"]}
+              </option>
+            ))}
+          </select>
+          <input
+            onChange={numberToSell}
+            className="text-black"
+            type="number"
+            value={sellAmount}
+          />
+        </div>
+
+        <div>
+          <div>
+            1 {leftSelection?.symbol} = ${leftSelection?.current_price}
+          </div>
+          <div>
+            {sellAmount} {leftSelection?.symbol} = $
+            {leftSelection?.current_price * sellAmount}
+          </div>
+        </div>
+      </div>
+      <div className="w-5/12 h-[150px] rounded-3xl p-5 bg-[#181825] mr-5">
+        <div>You buy</div>
+        <div className="flex justify-between">
+          <select onChange={setRight} className="w-24 text-black" name="" id="">
+            {marketData.map((coin) => {
+              if (coin.id === "ethereum") {
+                return (
+                  <option
+                    key={coin["id"]}
+                    className="text-black"
+                    value={coin["id"]}
+                    selected
+                  >
+                    {coin["name"]}
+                  </option>
+                );
+              } else {
+                return (
+                  <option
+                    key={coin["id"]}
+                    className="text-black"
+                    value={coin["id"]}
+                  >
+                    {coin["name"]}
+                  </option>
+                );
+              }
+            })}
+          </select>
+          <input
+            onChange={numberToBuy}
+            className="text-black"
+            type="number"
+            value={buyAmount}
+          />
+        </div>
+        <div>
+          1 {rightSelection?.symbol} = ${rightSelection?.current_price}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+Container.propTypes = {
+  marketData: PropTypes.node,
+};
 
 export default function Compare() {
   const [currentDate, setCurrentDate] = useState("");
   const [MarketData, setMarketData] = useState([]);
   const [ErrorMessage, setErrorMessage] = useState("");
-
-  const CompareBox = () => {
-    const [coinInfo, setCoinInfo] = useState("");
-
-    const setDisplayInfo = (coin) => {
-      setCoinInfo(coin.target.value);
-    };
-
-    return (
-      <div>
-        <select
-          onChange={setDisplayInfo}
-          className="w-24 text-black"
-          name=""
-          id=""
-        >
-          {MarketData.map((coin) => (
-            <option key={coin["id"]} className="text-black" value={coin["id"]}>
-              {coin["name"]}
-            </option>
-          ))}
-        </select>
-        <div>{coinInfo}</div>
-      </div>
-    );
-  };
 
   const getCoinData = async () => {
     const coinKey =
@@ -63,14 +154,8 @@ export default function Compare() {
         <div>Online currency convertor</div>
         <div>{currentDate}</div>
 
-        <div className="flex justify-center my-5">
-          <div className="w-5/12 h-[150px] rounded-3xl p-5 bg-[#181825] mr-5">
-            <CompareBox />
-          </div>
-          <div className="w-5/12 h-[150px] rounded-3xl p-5 bg-[#181825] ml-5">
-            <CompareBox />
-          </div>
-        </div>
+        <Container marketData={MarketData} />
+
         <div className="flex justify-center items-center h-[500px] rounded-3xl bg-[#181825]">
           Graph for value
         </div>
